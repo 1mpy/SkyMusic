63c199c7e6bf7bfeec7b9891ac8d8620204452a4
Anton <Impl3x@yandex.ru>
2023-07-08 19:02:24 +0300

    burger

--- src/components/main/navigation/burger/burger.jsx
+++ src/components/main/navigation/burger/burger.jsx
@@ -1,9 +1,25 @@
-export default function Burger() {
+import { useState } from 'react'
+import Menu from '../navMenu/NavMenu'
+
+export default function BurgerToMenu() {
+  const [visible, setVisible] = useState(true)
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
   return (
-    <div className="nav__burger burger">
+    <div
+      className="nav__burger burger"
+      onClick={toggleVisibility}
+      onKeyDown={toggleVisibility}
+      role="button"
+      tabIndex="0"
+    >
       <span className="burger__line" />
       <span className="burger__line" />
       <span className="burger__line" />
+      {!visible && <Menu />}
     </div>
   )
 }
+
--- src/components/main/navigation/nav/nav.jsx
+++ src/components/main/navigation/nav/nav.jsx
@@ -1,13 +1,12 @@
 import Logo from '../logo/logo'
 import Burger from '../burger/burger'
-import Menu from '../navMenu/NavMenu'
+// import Menu from '../navMenu/NavMenu'
 
 export default function Nav() {
   return (
     <nav className="main__nav nav">
       <Logo />
       <Burger />
-      <Menu />
     </nav>
   )
 }
